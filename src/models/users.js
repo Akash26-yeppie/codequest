4768f2802d1ee5147fb11c92fc39b399e819c2b8
Akash26-yeppie <gamesfiverr26@gmail.com>
2026-05-04 22:35:51 +0530

    Add files via upload

--- src/models/users.js
+++ src/models/users.js
@@ -1,1 +1,44 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
 
+const userSchema = new Schema({
+    firstName: {
+        type: String,
+        minLength: 2,
+        maxLength: 20,
+        require: true
+    },
+    lastName: {
+        type: String,
+        minLength: 2,
+        maxLength: 20,
+    },
+    emailId: {
+        type: String,
+        require: true,
+        unique: true,
+        lowercase: true,
+        immutable: true
+    },
+    age: {
+        type: Number,
+        min: 6,
+        max: 100,
+    },
+    role: {
+        type: String,
+        enum: ["user", "admin"],
+        default: "user"
+    },
+    problemSolved: {
+        type: [String]
+    },
+    password: {
+        type: String,
+        required: true
+    }
+});
+
+
+const User = mongoose.model("users", userSchema);
+module.exports = User;
